11019a862916962bdfae17424c510c729fb352d4
Elfsong <dumingzhe@126.com>
2025-03-26 10:13:18 +0000

    Fix a bug

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 cd src
+rm monolith.log
 /home/nus_cisco_wp1/miniconda3/envs/monolith/bin/gunicorn -w 1 -b 0.0.0.0:8000 -D --pid gunicorn.pid backend:app
 echo "Waiting for Gunicorn to start..."
 sleep 1
