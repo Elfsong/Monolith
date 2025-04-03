eee8a8b99d464acabe4c2eaf46cafc65b9de5d06
Elfsong <dumingzhe@126.com>
2025-04-03 18:15:42 +0000

    Update Stop script

--- stop.sh
+++ stop.sh
@@ -2,6 +2,7 @@ if [ -f src/gunicorn.pid ]; then
     echo "Stopping Gunicorn..."
     kill -9 $(cat src/gunicorn.pid)
     pkill gunicorn
+    ps ax | grep gunicorn | grep -v grep | awk '{print $1}' | sudo xargs -r kill -9
     rm src/monolith.log
     rm src/gunicorn.pid
     echo "Stopped Gunicorn."
