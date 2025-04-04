514676eceeb856222a7061c132cc163d464ee927
Elfsong <dumingzhe@126.com>
2025-04-04 08:56:34 +0000

    Polish the stop script

--- stop.sh
+++ stop.sh
@@ -1,10 +1,13 @@
+# Author: Du Mingzhe (mingzhe@nus.edu.sg)
+
 if [ -f src/gunicorn.pid ]; then
     echo "Stopping Gunicorn..."
-    kill -9 $(cat src/gunicorn.pid)
-    pkill gunicorn
-    ps ax | grep gunicorn | grep -v grep | awk '{print $1}' | sudo xargs -r kill -9
-    rm src/monolith.log
-    rm src/gunicorn.pid
+    kill -9 $(cat src/gunicorn.pid)             # Read the process ID from "src/gunicorn.pid" and forcefully kill that process.
+    pkill gunicorn                              # Kill any processes named "gunicorn" using the pkill command.
+    ps ax | grep gunicorn | grep -v grep | awk '{print $1}' | sudo xargs -r kill -9 # extract the process IDs of gunicorn, and use sudo to force kill them if needed.
+    docker rm $(docker ps -a -q)                # Remove all Docker containers (this command removes containers by listing all container IDs).
+    rm src/monolith.log                         # Remove the application log file.
+    rm src/gunicorn.pid                         # Remove the PID file since the process has been terminated.
     echo "Stopped Gunicorn."
 else
     echo "Gunicorn is not running."
